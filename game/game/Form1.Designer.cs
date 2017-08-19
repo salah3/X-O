ed19bb0703b29c26dcb7866e097dabb4a5c9b8c3
Mohamed Salah <30607210+salah3@users.noreply.github.com>
2017-08-19 19:51:06 +0200

    Add files via upload

---
+++ game/game/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttn1 = new System.Windows.Forms.Button();
+            this.bttn2 = new System.Windows.Forms.Button();
+            this.bttn3 = new System.Windows.Forms.Button();
+            this.bttn6 = new System.Windows.Forms.Button();
+            this.bttn5 = new System.Windows.Forms.Button();
+            this.bttn4 = new System.Windows.Forms.Button();
+            this.bttn9 = new System.Windows.Forms.Button();
+            this.bttn8 = new System.Windows.Forms.Button();
+            this.bttn7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bttn1
+            // 
+            this.bttn1.AutoSize = true;
+            this.bttn1.BackColor = System.Drawing.Color.Purple;
+            this.bttn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn1.FlatAppearance.BorderSize = 0;
+            this.bttn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn1.Font = new System.Drawing.Font("Book Antiqua", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttn1.ForeColor = System.Drawing.Color.White;
+            this.bttn1.Location = new System.Drawing.Point(95, 83);
+            this.bttn1.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn1.Name = "bttn1";
+            this.bttn1.Size = new System.Drawing.Size(213, 163);
+            this.bttn1.TabIndex = 0;
+            this.bttn1.UseVisualStyleBackColor = false;
+            // 
+            // bttn2
+            // 
+            this.bttn2.AutoSize = true;
+            this.bttn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn2.FlatAppearance.BorderSize = 0;
+            this.bttn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn2.ForeColor = System.Drawing.Color.Black;
+            this.bttn2.Location = new System.Drawing.Point(298, 83);
+            this.bttn2.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn2.Name = "bttn2";
+            this.bttn2.Size = new System.Drawing.Size(213, 163);
+            this.bttn2.TabIndex = 1;
+            this.bttn2.UseVisualStyleBackColor = true;
+            // 
+            // bttn3
+            // 
+            this.bttn3.AutoSize = true;
+            this.bttn3.BackColor = System.Drawing.Color.Purple;
+            this.bttn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn3.FlatAppearance.BorderSize = 0;
+            this.bttn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn3.Font = new System.Drawing.Font("Book Antiqua", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttn3.ForeColor = System.Drawing.Color.White;
+            this.bttn3.Location = new System.Drawing.Point(502, 83);
+            this.bttn3.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn3.Name = "bttn3";
+            this.bttn3.Size = new System.Drawing.Size(213, 163);
+            this.bttn3.TabIndex = 2;
+            this.bttn3.UseVisualStyleBackColor = false;
+            // 
+            // bttn6
+            // 
+            this.bttn6.AutoSize = true;
+            this.bttn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn6.FlatAppearance.BorderSize = 0;
+            this.bttn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn6.ForeColor = System.Drawing.Color.Black;
+            this.bttn6.Location = new System.Drawing.Point(502, 236);
+            this.bttn6.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn6.Name = "bttn6";
+            this.bttn6.Size = new System.Drawing.Size(213, 163);
+            this.bttn6.TabIndex = 3;
+            this.bttn6.UseVisualStyleBackColor = true;
+            // 
+            // bttn5
+            // 
+            this.bttn5.AutoSize = true;
+            this.bttn5.BackColor = System.Drawing.Color.Purple;
+            this.bttn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn5.FlatAppearance.BorderSize = 0;
+            this.bttn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn5.Font = new System.Drawing.Font("Book Antiqua", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttn5.ForeColor = System.Drawing.Color.White;
+            this.bttn5.Location = new System.Drawing.Point(298, 236);
+            this.bttn5.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn5.Name = "bttn5";
+            this.bttn5.Size = new System.Drawing.Size(213, 163);
+            this.bttn5.TabIndex = 4;
+            this.bttn5.UseVisualStyleBackColor = false;
+            // 
+            // bttn4
+            // 
+            this.bttn4.AutoSize = true;
+            this.bttn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn4.FlatAppearance.BorderSize = 0;
+            this.bttn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn4.ForeColor = System.Drawing.Color.Black;
+            this.bttn4.Location = new System.Drawing.Point(95, 236);
+            this.bttn4.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn4.Name = "bttn4";
+            this.bttn4.Size = new System.Drawing.Size(213, 163);
+            this.bttn4.TabIndex = 5;
+            this.bttn4.UseVisualStyleBackColor = true;
+            // 
+            // bttn9
+            // 
+            this.bttn9.AutoSize = true;
+            this.bttn9.BackColor = System.Drawing.Color.Purple;
+            this.bttn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn9.FlatAppearance.BorderSize = 0;
+            this.bttn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn9.Font = new System.Drawing.Font("Book Antiqua", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttn9.ForeColor = System.Drawing.Color.White;
+            this.bttn9.Location = new System.Drawing.Point(502, 391);
+            this.bttn9.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn9.Name = "bttn9";
+            this.bttn9.Size = new System.Drawing.Size(213, 163);
+            this.bttn9.TabIndex = 6;
+            this.bttn9.UseVisualStyleBackColor = false;
+            // 
+            // bttn8
+            // 
+            this.bttn8.AutoSize = true;
+            this.bttn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn8.FlatAppearance.BorderSize = 0;
+            this.bttn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn8.ForeColor = System.Drawing.Color.Black;
+            this.bttn8.Location = new System.Drawing.Point(298, 391);
+            this.bttn8.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn8.Name = "bttn8";
+            this.bttn8.Size = new System.Drawing.Size(213, 163);
+            this.bttn8.TabIndex = 7;
+            this.bttn8.UseVisualStyleBackColor = true;
+            // 
+            // bttn7
+            // 
+            this.bttn7.AutoSize = true;
+            this.bttn7.BackColor = System.Drawing.Color.Purple;
+            this.bttn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttn7.FlatAppearance.BorderSize = 0;
+            this.bttn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttn7.Font = new System.Drawing.Font("Book Antiqua", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttn7.ForeColor = System.Drawing.Color.White;
+            this.bttn7.Location = new System.Drawing.Point(95, 391);
+            this.bttn7.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.bttn7.Name = "bttn7";
+            this.bttn7.Size = new System.Drawing.Size(213, 163);
+            this.bttn7.TabIndex = 8;
+            this.bttn7.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(786, 608);
+            this.Controls.Add(this.bttn7);
+            this.Controls.Add(this.bttn8);
+            this.Controls.Add(this.bttn9);
+            this.Controls.Add(this.bttn4);
+            this.Controls.Add(this.bttn5);
+            this.Controls.Add(this.bttn6);
+            this.Controls.Add(this.bttn3);
+            this.Controls.Add(this.bttn2);
+            this.Controls.Add(this.bttn1);
+            this.Font = new System.Drawing.Font("Book Antiqua", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttn1;
+        private System.Windows.Forms.Button bttn2;
+        private System.Windows.Forms.Button bttn3;
+        private System.Windows.Forms.Button bttn6;
+        private System.Windows.Forms.Button bttn5;
+        private System.Windows.Forms.Button bttn4;
+        private System.Windows.Forms.Button bttn9;
+        private System.Windows.Forms.Button bttn8;
+        private System.Windows.Forms.Button bttn7;
+    }
+}
+
---
+++ game/game/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        enum GameType { x , o };
+        GameType start_game = GameType.x;
+        int numOf_played = 0;
+        Button[,] btns = new Button[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+            btns[0, 0] = bttn1;
+            btns[0, 1] = bttn2;
+            btns[0, 2] = bttn3;
+            btns[1, 0] = bttn4;
+            btns[1, 1] = bttn5;
+            btns[1, 2] = bttn6;
+            btns[2, 0] = bttn7;
+            btns[2, 1] = bttn8;
+            btns[2, 2] = bttn9;
+
+            foreach (Button btn in btns)
+
+                btn.Click += new EventHandler(btn_Click);
+
+        }
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text != "")
+                return;
+            numOf_played++;
+            btn.Text = start_game.ToString();
+            if (theWineer())
+            {
+                MessageBox.Show(" player "   +   start_game.ToString() +  "  is win  ");
+                NewGame();
+                return;
+            }
+            if (numOf_played == 9)
+            {
+                MessageBox.Show("the players equals");
+                NewGame();
+            }
+            anotherGame();
+        }
+
+        void NewGame ()
+        {
+            foreach (Button btn in btns)
+                btn.Text = "";
+            numOf_played = 0; 
+
+ 
+        }
+        void anotherGame()
+        {
+            if (start_game == GameType.x)
+                start_game = GameType.o;
+            else
+                start_game = GameType.x;
+
+        }
+        bool theWineer()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+
+                if (btns[0, i].Text == start_game.ToString() 
+                    && btns[0, i].Text == btns[1, i].Text
+                    &&
+                    btns[1, i].Text == btns[2, i].Text)
+                    return true;
+
+                if (btns[i, 0].Text == start_game.ToString() && btns[i, 0].Text == btns[i, 1].Text &&
+                    btns[i,1].Text == btns[i,2].Text)
+                    return true;
+
+            }
+
+            if (btns[0, 0].Text == start_game.ToString()
+                && btns[0, 0].Text == btns[1, 1].Text
+                &&
+                btns[1, 1].Text == btns[2, 2].Text)
+                return true;
+
+            if (btns[0, 2].Text == start_game.ToString()
+                && btns[0, 2].Text == btns[1, 1].Text
+                &&
+                btns[1, 1].Text == btns[2, 0].Text)
+                return true;
+            
+                return false;
+
+
+
+        }
+
+       
+        
+    
+        
+    }
+}
